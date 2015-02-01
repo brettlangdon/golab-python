35eae0ff2f40c857b6a2aa7692af6d700d668b42
brettlangdon <brett@blangdon.com>
2015-02-01 16:04:38 -0500

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("./requirements.txt") as fp:
 
 setup(
     name="golab",
-    version="0.1.1",
+    version="0.1.2",
     description="Python client for GoLab",
     author="Brett Langdon",
     author_email="brett@blangdon.com",
